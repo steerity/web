f483ff09ec49558d65d05acaf462dbd367b5c100
patricklouvel <patrick.louvel@gmail.com>
2016-09-29 16:33:47 +0200

    1 er upload
    installation du site

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+
+// configure
+$from = 'Demo contact form <steerity.faq@gmail.com>';
+$sendTo = 'Demo contact form <steerity.faq@gmail.com>';
+$subject = 'Nouveau message du contact form';
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
+$okMessage = "Votre message a bien été envoyé. Merci, nous vous repondrons très rapidement!";
+$errorMessage = "Une erreur est survenue. Essayer à nouveau s'il vous plait";
+
+// let's do the sending
+
+try
+{
+    $emailText = "You have new message from contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    mail($sendTo, $subject, $emailText, "From: " . $from);
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+else {
+    echo $responseArray['message'];
+}
